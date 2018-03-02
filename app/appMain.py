d679365e892e338abf295d717f90843d5fb666af
Coop Coding <coop.coding@gmail.com>
2018-03-02 12:47:53 +1100

    Testing linters

--- app/appMain.py
+++ app/appMain.py
@@ -13,3 +13,19 @@ def main():
     x = 'a'
     _x = 'a'
     _ = 'a'
+
+
+# define the name of the file to read from
+filename = "test.txt"
+
+# open the file for reading
+filehandle = open(filename, 'r')
+while True:
+    # read a single line
+    line = filehandle.readline()
+    if not line:
+        break
+    print(line)
+
+# close the pointer to that file
+filehandle.close()
